bde640a5304e5410cd30669c4ed1aa5475de644e
Navin Paudel <35531592+Navinim@users.noreply.github.com>
2023-01-16 23:40:18 +0545

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
 import styled from "styled-components";
 import { mobile } from "../responsive";
 import Product from './Product';
